526f96c6f63cc149800b2b8f0badcb26603531ef
Katherine Wren <kwren@outlook.com>
2019-04-23 16:48:04 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name=(string)
+    @name = string
+  end
+  def name
+    @name
+  end
+  def breed=(string)
+    @breed = string
+  end
+  def breed
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name=(string)
+    @name = string
+  end
+  def name
+    @name
+  end
+  def job=(string)
+    @job = string
+  end
+  def job
+    @job
+  end
+end
