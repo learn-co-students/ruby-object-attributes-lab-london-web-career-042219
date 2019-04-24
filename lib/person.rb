7e635fbc68c11547dc50f5ea35d51b765f59de2c
placid-computer-9089 <zubairmaqsood866@gmail.com>
2019-04-24 18:48:19 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person 
+  def initialize(name)
+    @name = name 
+    @job = job
+  end 
+  
+  def name 
+    @name 
+  end 
+  
+  def name = (same_name)
+    @name = same_name
+  end 
+  
+  def job 
+    @job 
+  end 
+  
+  def job = (same_job)
+    @job = same_job
+  end
+  
+end 
