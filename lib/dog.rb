23c4052558da024d865f3c3d4adf2f50e0ebc7c5
rossmawd <rossmawd@gmail.com>
2019-04-24 09:05:34 +0100

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def name
+    @name    # name is just reading the @name
+  end
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  
+end
---
+++ lib/person.rb
@@ -0,0 +1,25 @@
+class Person
+  def name
+    @name
+  end
+
+  def name=(person_name)
+    @name = person_name
+  end
+
+  def job=(person_job)
+    @job = person_job
+
+  end
+
+  def job
+    @job
+  end
+
+
+
+
+end
+
+beyonce = Person.new
+beyonce.job = "Singer"
